a3df0eca97d717ff0134e3be083631e0e961b47c
mitsu0428 <mitsuprograming@gmail.com>
2023-05-20 18:34:22 +0900

    fix: 現在のディレクトリの一個上にappディレクトリを生成するように

---
+++ boiler-next-app.sh
@@ -0,0 +1,25 @@
+# 引数を受け取る
+# $1: app-name
+
+cd ..
+
+# /app-nameディレクトリがなければ作成
+if [ ! -d $1 ]; then
+    mkdir $1
+fi
+
+# ディレクトリ移動
+cd $1
+
+# ファイルがなければ作成
+npx create-next-app $1-front --typescript
+
+# ディレクトリ移動
+cd $1-front
+
+# パッケージインストール
+yarn
+yarn add styled-components @types/styled-components
+yarn add babel-plugin-styled-components
+yarn add react-icons
+yarn add @mui/material @emotion/react @emotion/styled
